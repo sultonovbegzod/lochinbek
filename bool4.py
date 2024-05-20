2c9ac707648bb4cfcd861fead4e9aaf797a32ab6
sultonovbegzod <sultonovbegzod684@gmail.com>
2024-05-20 20:00:03 +0500

    Add files via upload

---
+++ bool4.py
@@ -0,0 +1,5 @@
+a = 6
+b = 5
+
+x = a > 2 and b <= 3
+print(x)
---
+++ if29.py
@@ -0,0 +1,12 @@
+a = 0
+
+if a > 0:
+    print("musbat toq son" )
+elif a > 0:
+        ("musbat juft son")
+if a == 0:
+    print("0 ga teng")
+if a < 0:
+    print("manfiy juft son")
+elif a < 0:
+    print("manfiy toq son")
